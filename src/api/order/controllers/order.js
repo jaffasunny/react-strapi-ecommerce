228ddca68c49c9ca951d1e26d03ae6397f7d7edf
Syed Jaffer Sunny <52747254+jaffasunny@users.noreply.github.com>
2022-05-11 00:57:46 -0700

    Added about order schema

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- *  about controller
+ *  order controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::about.about');
+module.exports = createCoreController('api::order.order');
